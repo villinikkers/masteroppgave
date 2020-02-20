ae13bd62c2223a051bae187238a416cda1cf059f
Christian <christian@cke.no>
2020-02-20 16:07:46 +0100

    oppdatert kommentering

--- alarmbox/send_serial_read_LSL.py
+++ alarmbox/send_serial_read_LSL.py
@@ -12,14 +12,15 @@ outlet = StreamOutlet(info)
 # To find out which port (COMx) the alarmbox is connected to run the windows
 # 'Device manager'.
 #
-# This code-block listenes for input on the serial port, there are two types
+# The following code-block listens for input on the serial port, there are two types
 # of events that send markers to the serial port:
 #
 #   1. Triggering of an alarm sends "alarm"
-#   2. The toggle switch is flipped: sends the reaction time in ms (e.g. 1400)
+#   2. The toggle switch is flipped: sends the reaction time in ms as measured
+#       by the arduino(e.g. 1400)
 #
 # The code tries to read data from the serial port continously.
-# If it recieves a signal corresponding to a trigger-event or reaction-event
+# If it recieves a signal corresponding to a trigger-event, or reaction-event
 # it pushes the corresponding marker through the LSL-outlet.
 
 with serial.Serial('COM5', 9600, timeout=0.005) as ser:
