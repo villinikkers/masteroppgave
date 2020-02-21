78cf25b6af2e7b5269e4b17103edf001c5250152
Christian <christian@cke.no>
2020-02-21 10:42:25 +0100

    Sync alarmbox-lsl med dropbox versjon.

--- alarmbox/send_serial_read_LSL.py
+++ alarmbox/send_serial_read_LSL.py
@@ -27,37 +27,41 @@ outlet = StreamOutlet(info)
 # To find out which port (COMx) the alarmbox is connected to run the windows
 # 'Device manager'.
 #
-# The following code-block listens for input on the serial port, there are two types
+# This code-block listenes for input on the serial port, there are two types
 # of events that send markers to the serial port:
 #
 #   1. Triggering of an alarm sends "alarm"
-#   2. The toggle switch is flipped: sends the reaction time in ms as measured
-#       by the arduino(e.g. 1400)
+#   2. The toggle switch is flipped: sends the reaction time in ms (e.g. 1400)
 #
 # The code tries to read data from the serial port continously.
-# If it recieves a signal corresponding to a trigger-event, or reaction-event
+# If it recieves a signal corresponding to a trigger-event or reaction-event
 # it pushes the corresponding marker through the LSL-outlet.
 
-with serial.Serial(port, 9600, timeout=0.005) as ser:
-  # Make sure iMotions starts to collect markers (press "play" on the sensor in
-  # iMotions during the following startup sequence):
-  print("Sending startup markers...")
-  try:
-    for i in range(4):
-      outlet.push_sample(['starting'])
-      time.sleep(5)
-  except:
-    print("ERROR: Start sequence failed.")
+try:
+    with serial.Serial(port, 9600, timeout=0.005) as ser:
+      # Make sure iMotions starts to collect markers (press "play" on the sensor in
+      # iMotions during the following startup sequence):
+      print("Sending startup markers...")
+      try:
+        for i in range(4):
+          outlet.push_sample(['starting'])
+          time.sleep(5)
+      except:
+        print("ERROR: Start sequence failed.")
 
-  # Normal operating mode
-  print("running....")
-  while True:
-    line = ser.readline().decode('ascii')
-    try:
-        intLine = int(line)
-        line += " ms"
-        outlet.push_sample([line])
-    except:
-        if(line=='alarm'):
+      # Normal operating mode
+      print("running....")
+      while True:
+        line = ser.readline().decode('ascii')
+        try:
+            intLine = int(line)
+            line += " ms"
             outlet.push_sample([line])
-    ser.reset_input_buffer()
+        except:
+            if(line=='alarm'):
+                outlet.push_sample([line])
+        ser.reset_input_buffer()
+except serial.serialutil.SerialException:
+    print(f"""Could not open port {port}, make sure you have entered correct port number and try again.""")
+finally:
+    print("Program aborted.")
